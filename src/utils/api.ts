99adc6c623799ed767ed5b900b9e278a42f17b2f
JH9min <jh9min@gmail.com>
2025-04-24 23:14:38 +0800

    250424_替換為nginx port號
    加入nginx，將原本打向後端的url轉向nginx

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 // This file contains utility functions for API calls and cookie management.
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7259/api';
+export const API_BASE = import.meta.env.VITE_API_BASE_URL // || 'https://localhost:7259/api';
 
 export function getCookie(name: string): string | null {
   const match = document.cookie.match(new RegExp('(?:^|; )' + name + '=([^;]*)'));
